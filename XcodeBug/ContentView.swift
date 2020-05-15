8fd6982b2c11e2e2b8ecf12d172218ab489db2eb
Max Desiatov <max@desiatov.com>
2020-05-15 17:17:16 +0100

    Initial Commit

---
+++ XcodeBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeBug
+//
+//  Created by Max Desiatov on 15/05/2020.
+//  Copyright © 2020 Digital Signal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
